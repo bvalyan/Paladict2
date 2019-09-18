b497c9bc4cc5789a53830f6efe83ae80c4eea8e2
Brandon Valyan-Clark <brandon.valyan-clark@delta.com>
2019-09-18 19:12:42 -0400

    Added abbility recyclerview. Unbound nav from data

--- app/src/main/java/com/example/paladict2/view/mainviewpager/champions/ChampionDetailFragment.kt
+++ app/src/main/java/com/example/paladict2/view/mainviewpager/champions/ChampionDetailFragment.kt
@@ -6,41 +6,41 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.navArgs
-import com.example.paladict2.Constants.Companion.PALADINS_SELECTED_CHAMPION
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.SnapHelper
 import com.example.paladict2.R
 import com.example.paladict2.databinding.ChampionDetailPageBinding
-import com.example.paladict2.model.Champion
 import com.example.paladict2.viewmodel.ChampionViewModel
-import com.example.paladict2.viewmodel.ChampionViewModelFactory
+import kotlinx.android.synthetic.main.champion_detail_page.*
 
 class ChampionDetailFragment : Fragment() {
-    private lateinit var championViewModel: ChampionViewModel
-
-    companion object {
-        fun newInstance(title: String, champion : Champion): Fragment {
-            val fragment = ChampionDetailFragment()
-            val args = Bundle()
-            args.putSerializable(PALADINS_SELECTED_CHAMPION, champion)
-            args.putString("title", title)
-            fragment.arguments = args
-            return fragment
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val args : ChampionDetailFragmentArgs by navArgs()
-        val binding : ChampionDetailPageBinding = DataBindingUtil.inflate(inflater, R.layout.champion_detail_page, container, false)
+        val args: ChampionDetailFragmentArgs by navArgs()
+        val binding: ChampionDetailPageBinding =
+            DataBindingUtil.inflate(inflater, R.layout.champion_detail_page, container, false)
         val myView = binding.root
         val selectedChampion = args.champion
         val champion = ChampionViewModel(selectedChampion)
         binding.champViewModel = champion
 
         return myView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = ChampionAbilityRecyclerAdapter()
+        val snapHelper = PagerSnapHelper()
+        snapHelper.attachToRecyclerView(champion_ability_recycler)
+        champion_ability_recycler.layoutManager = LinearLayoutManager(context)
+        champion_ability_recycler.adapter = adapter
     }
 }
